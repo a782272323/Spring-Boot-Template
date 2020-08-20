fd634ba2544c63a91ceed659413df79591139262
a782272323 <782272323@qq.com>
2020-08-20 22:29:41 +0800

    修复通过切面aop打印日志的bug

--- AOP/loging/src/main/java/learn/lhb/aop/logging/ascept/LogAspect.java
+++ AOP/loging/src/main/java/learn/lhb/aop/logging/ascept/LogAspect.java
@@ -7,7 +7,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
